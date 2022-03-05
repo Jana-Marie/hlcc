afaeb080f636331e04d83a82e29daa67100eea98
Jana-Marie <janamarie@janamarie.dev>
2022-03-05 02:57:45 +0100

    optimized a bit, should work on mobile now

--- www/index.js
+++ www/index.js
@@ -5,16 +5,11 @@ textBox.addEventListener('keypress', function(){
     if(event.which == 13){
       let res = compute_input(document.getElementById('hormone_input').value);
       if (res != -1.0){
-        document.getElementById("hormone_output").innerHTML = "computes to: " + Math.round((res + Number.EPSILON) * 100) / 100;
+        document.getElementById("hormone_output").innerHTML = "computes to: " + Math.round((res + Number.EPSILON) * 1000) / 1000;
       } else {
         document.getElementById("hormone_output").innerHTML = "Input error!";
       }
-      //document.getElementById("hormone_output").classList.remove("fade-out");
       document.getElementById("hormone_output").classList.add("fade-in");
       console.log(res);
-    }
-    else if(event.which == 8 || event.which == 13){ //if (document.getElementById('hormone_input').value == "")  // why does this not work :(
-      document.getElementById("hormone_output").classList.add("fade-out");
-      document.getElementById("hormone_output").classList.remove("fade-in");
     }
 });
