23fc7c2e743718a02d449888f3f3ea3297c08785
Jana-Marie <janamarie@janamarie.dev>
2022-03-05 23:33:12 +0100

    added pretty output

--- www/index.js
+++ www/index.js
@@ -4,11 +4,7 @@ var textBox = document.getElementById('hormone_input');
 textBox.addEventListener('keypress', function(){
     if(event.which == 13){
       let res = compute_input(document.getElementById('hormone_input').value);
-      if (res != -1.0){
-        document.getElementById("hormone_output").innerHTML = "computes to: " + Math.round((res + Number.EPSILON) * 1000) / 1000;
-      } else {
-        document.getElementById("hormone_output").innerHTML = "Input error!";
-      }
+      document.getElementById("hormone_output").innerHTML = res
       document.getElementById("hormone_output").classList.add("fade-in");
       console.log(res);
     }
